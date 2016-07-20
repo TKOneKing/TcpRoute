8174fd911b5db043824e1acd2de009b77f4d76a9
GameXG <GameXG@Gmail.Com>
2016-07-20 08:49:57 +0800

    部分代码

---
+++ netchan/dnschan_dns.go
@@ -0,0 +1,113 @@
+package netchan
+
+import (
+	"net"
+	"github.com/miekg/dns"
+	"time"
+	"log"
+)
+
+type dnsServer struct {
+	Credit int //dns服务器信誉，根据信誉不同，查询得到的dns记录信誉也不同。
+}
+
+type ConfigDnsServer struct {
+	Addr string
+	Credit int
+}
+
+type dnsDns struct {
+	servers map[net.UDPAddr]*dnsServer
+}
+
+func NewDnsDns()(*dnsDns){
+	return &dnsDns{
+		servers:make(map[net.UDPAddr]*dnsServer),
+	}
+}
+
+func(d*dnsDns)setServers(servers []ConfigDnsServer){
+
+}
+
+
+
+func (d*dnsDns)query(domain string, RecordChan chan *DnsRecord, ExitChan chan int) {
+	dnschanDnsQuery(d.servers,domain,RecordChan,ExitChan)
+}
+
+func dnschanDnsQuery(servers map[net.UDPAddr]*dnsServer,domain string, RecordChan chan *DnsRecord, ExitChan chan int) {
+	select {
+	case <-ExitChan:
+		return
+	default:
+	}
+
+	// 打开端口
+	conn, err := net.ListenUDP("udp", nil)
+	if err != nil {
+		log.Printf("为dns请求打开udp失败，%v", err)
+		return
+	}
+	defer conn.Close()
+
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+
+	// dns 请求
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(domain), dns.TypeA)
+	m.RecursionDesired = true
+	mData, err := m.Pack()
+	if err != nil {
+		log.Printf("生成dns请求包失败，%v", err)
+		return
+	}
+
+	// 另开一个线程发出查询
+	go func() {
+		for k, _ := range servers {
+			if _, err := conn.WriteToUDP(mData, k); err != nil {
+				log.Printf("向%v发送dns请求失败，%v", k, err)
+			}
+		}
+	}()
+
+	// 接收查询结果并输入到信道
+	buf := make([]byte, 1500)
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			return
+		}
+		nbuf := buf[:n]
+
+		r := new(dns.Msg)
+		if err := r.Unpack(nbuf); err != nil {
+			log.Printf("解 DNS 包失败,%v", err)
+			continue
+		}
+
+		if r.Id != m.Id {
+			log.Printf("错误的dns id。")
+			continue
+		}
+
+		v := servers[*addr]
+		if v == nil {
+			log.Printf("未知的服务器 %v 回应，忽略。", *addr)
+			continue
+		}
+
+		for _, a := range r.Answer {
+			dnsA, err := a.(*dns.A)
+			if err != nil || dnsA == nil {
+				log.Printf("内部错误，a=%v,err=%v", dnsA, err)
+			}
+			RecordChan <- &DnsRecord{
+				Ip:dnsA.A.String(),
+				Credit:v.Credit,
+			}
+		}
+	}
+}
+
